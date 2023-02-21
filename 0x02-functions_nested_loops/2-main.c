5941765d97dea1855ff803ab61ea0b62c8b48cf3
Awesome Fabian <awekordi1@gmail.com>
2023-02-21 10:41:16 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check if char is lowercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
